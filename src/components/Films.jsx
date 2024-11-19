fc78443c378bffa2bb5d8c93c4805400d4557177
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2024-11-19 15:44:31 +0100

    fix css in Films.jsx

--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -10,7 +10,7 @@ const Films = ({films, onVote, hasVoted}) => {
           <div className="content-item" key={film._id}>
             <h2 className="content-title">{film.title}</h2>
             <ContentInfo category={film.category} tags={film.tags} />
-            {film.videoUrl && <video controls src={`http://localhost:3000${film.videoUrl}`} alt={film.title} className="content-film" />}
+            {film.videoUrl && <video controls src={`http://localhost:3000${film.videoUrl}`} alt={film.title} className="content-video" />}
             <div className="voting-container">
               <p>Upvotes: {film.upvotes} | Downvotes: {film.downvotes}</p>
               <button className="upvote-button" onClick={() => onVote(1)} disabled={hasVoted}>+</button>
